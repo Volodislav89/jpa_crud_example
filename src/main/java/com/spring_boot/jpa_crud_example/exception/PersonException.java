0620b9788af50d779ddf7bb14ebb77f3ddccd0ac
“Volodislav89” <shbloodislav@gmail.com>
2020-10-28 12:04:39 +0200

    Person and Emplyee exception classes

---
+++ src/main/java/com/spring_boot/jpa_crud_example/exception/PersonException.java
@@ -0,0 +1,7 @@
+package com.spring_boot.jpa_crud_example.exception;
+
+public class PersonException extends Exception {
+    public PersonException(String message) {
+        super(message);
+    }
+}
